c9adf3998833e5ccb3d23e277496777c3bb43cbf
Alexader <tyxj58@gmail.com>
2021-05-10 13:45:50 +0800

    fix(example): fix program error in solidity contracts

--- example/transfer.sol
+++ example/transfer.sol
@@ -99,12 +99,12 @@ contract Transfer {
     }
 }
 
-abstract contract Broker {
+contract Broker {
     function emitInterchainEvent(
         address destChainID,
         string memory destAddr,
         string memory funcs,
         string memory args,
         string memory argsCb,
-        string memory argsRb) virtual public;
+        string memory argsRb) public;
 }
